ebff9061bda60e0822062f8782b881181807328b
Sietse de Kaper <targeter@gmail.com>
2024-12-09 22:31:25 +0100

    Day 8,9

--- src/main/scala/aoc2024/Day08.scala
+++ src/main/scala/aoc2024/Day08.scala
@@ -3,13 +3,63 @@ package aoc2024
 import shared._
 
 case object Day08 extends AocTools(day = 8) {
-  def step1(lines: Seq[String]): Int = ???
+  type Grid = Map[Pos, Char]
 
-  def step2(lines: Seq[String]): Int =  ???
+  case class Pos(x: Int, y: Int) {
+    def -(other: Pos): Pos = Pos(x - other.x, y - other.y)
+    def +(other: Pos): Pos = Pos(x + other.x, y + other.y)
+  }
+
+  def parse(input: Seq[String]): Grid = {
+    input.zipWithIndex.flatMap { case (line, y) =>
+      line.zipWithIndex.map { case (c, x) =>
+        Pos(x, y) -> c
+      }
+    }.toMap
+  }
+
+  def antinodes(antennas: Seq[Pos]): Set[Pos] = {
+    val nodes = for {
+      a   <- antennas
+      b   <- antennas
+      if a != b
+      diff = b - a
+    } yield {
+      Seq(a - diff, b + diff)
+    }
+
+    nodes.flatten.toSet
+  }
+
+  def antinodes2(grid: Grid, antennas: Seq[Pos]): Set[Pos] = {
+    val nodes = for {
+      a   <- antennas
+      b   <- antennas
+      if a != b
+    } yield {
+      val diff = b - a
+      LazyList.iterate(a)(_ - diff).takeWhile(grid.contains) ++
+      LazyList.iterate(b)(_ + diff).takeWhile(grid.contains)
+    }
+    nodes.flatten.toSet
+
+  }
+
+  def step1(input: Seq[String]): Int = {
+    val grid        = parse(input)
+    val antennaSets = grid.filterNot(_._2 == '.').groupMap(_._2)(_._1).values.map(_.toSeq)
+    val all         = antennaSets.flatMap(antinodes).toSet
+    all.count(grid.contains)
+  }
+
+  def step2(grid: Grid): Int = {
+    val antennaSets = grid.filterNot(_._2 == '.').groupMap(_._2)(_._1).values.map(_.toSeq)
+    antennaSets.flatMap(antinodes2(grid, _)).toSet.count(grid.contains)
+  }
 
   def main(args: Array[String]): Unit = {
-    println("Step 1: " + step1(inputLines))
-    println("Step 2: " + step2(inputLines))
+//    val grid = parse(lines)
+//    println("Step 1: " + step1(inputLines))
+//    println("Step 2: " + step2(inputLines))
   }
 }
-
--- src/main/scala/aoc2024/Day09.scala
+++ src/main/scala/aoc2024/Day09.scala
@@ -2,14 +2,107 @@ package aoc2024
 
 import shared._
 
-case object Day09 extends AocTools(day = 9) {
-  def step1(lines: Seq[String]): Int = ???
+import scala.annotation.tailrec
 
-  def step2(lines: Seq[String]): Int =  ???
+case object Day09 extends AocTools(day = 9) {
+  sealed trait DiskBlock {
+    def size: Int
+  }
+  case class FreeBlock(size: Int)          extends DiskBlock {
+    def fit(fileBlock: FileBlock): (Vector[DiskBlock] => Vector[DiskBlock], DiskBlock) = {
+      if (size == fileBlock.size) (identity, fileBlock)
+      else if (size > fileBlock.size) ((blocks) => FreeBlock(size - fileBlock.size) +: blocks, fileBlock)
+      else
+        (
+          (blocks) => blocks :+ FileBlock(id = fileBlock.id, size = fileBlock.size - this.size),
+          FileBlock(id = fileBlock.id, size = this.size)
+        )
+    }
+
+    def fit2(fileBlock: FileBlock): Vector[DiskBlock] = {
+      if (fileBlock.size == size) Vector(fileBlock)
+      else Vector(fileBlock, FreeBlock(size - fileBlock.size))
+    }
+  }
+  case class FileBlock(id: Int, size: Int) extends DiskBlock
+
+  def parse(diskMap: Seq[Int]): Vector[DiskBlock] = {
+    val evenSize      = if (diskMap.size % 2 == 0) diskMap.size else diskMap.size + 1
+    val (files, free) = diskMap
+      .padTo(evenSize, 0)
+      .sliding(2, 2)
+      .map { case Seq(a, b) => (a, b) }
+      .toVector
+      .unzip
+
+    val fileBlocks = files.zipWithIndex.map { case (f, i) => FileBlock(i, f) }
+    val freeBlocks = free.map(FreeBlock(_))
+    fileBlocks
+      .zip(freeBlocks)
+      .flatMap(x => Vector(x._1, x._2))
+  }
+
+  @tailrec
+  def defrag(diskMap: Vector[DiskBlock], newDiskMap: Vector[DiskBlock] = Vector.empty): Vector[DiskBlock] = {
+    diskMap match {
+      case dm if dm.isEmpty                               => newDiskMap
+      case (file: FileBlock) +: rest                      => defrag(rest, newDiskMap :+ file)
+      case (free: FreeBlock) +: rest :+ (_: FreeBlock)    => defrag(free +: rest, newDiskMap)
+      case (free: FreeBlock) +: rest :+ (file: FileBlock) =>
+        val (remainder, placed) = free.fit(file)
+        defrag(remainder(rest), newDiskMap :+ placed)
+      case (free: FreeBlock) +: rest                      => defrag(rest, newDiskMap :+ free) // free block at the end
+    }
+  }
+
+  @tailrec
+  def defrag2(diskMap: Vector[DiskBlock], newDiskMap: Vector[DiskBlock] = Vector.empty): Vector[DiskBlock] = {
+    if (diskMap.isEmpty) newDiskMap
+    else {
+      val (rest :+ last) = diskMap
+
+      diskMap.last match {
+        case FreeBlock(size) => defrag2(rest, FreeBlock(size) +: newDiskMap)
+        case file: FileBlock =>
+          val indexOfFirstFit = rest.indexWhere {
+            case FreeBlock(size) => size >= file.size
+            case _               => false
+          }
+
+          if (indexOfFirstFit == -1) defrag2(rest, file +: newDiskMap)
+          else {
+            val before = rest.slice(0, indexOfFirstFit)
+            val free   = rest(indexOfFirstFit).asInstanceOf[FreeBlock]
+            val after  = rest.slice(indexOfFirstFit + 1, rest.size)
+            defrag2(before ++ free.fit2(file) ++ after, FreeBlock(file.size) +: newDiskMap)
+          }
+
+      }
+    }
+
+  }
+
+  def checksum(filesystem: Vector[DiskBlock]): Long = {
+    filesystem
+      .flatMap {
+        case FileBlock(id, size) => Vector.fill(size)(id.toLong)
+        case FreeBlock(size)     => Vector.fill(size)(0L)
+      }
+      .zipWithIndex
+      .map { case (id, i) => id * i.toLong }
+      .sum
+  }
+
+  def step1(line: String): Long = {
+    val diskMap = parse(line.map(_.asDigit))
+    val blocks  = defrag(diskMap)
+    checksum(blocks)
+  }
+
+  def step2(line: String): Long = checksum(defrag2(parse(line.map(_.asDigit))))
 
   def main(args: Array[String]): Unit = {
-    println("Step 1: " + step1(inputLines))
-    println("Step 2: " + step2(inputLines))
+    println("Step 1: " + step1(inputLines.head)) // 6200294120911
+    println("Step 2: " + step2(inputLines.head)) // 6227018762750
   }
 }
-
